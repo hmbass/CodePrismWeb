61b7024f3314237c05c3ef79c5819944ccdb7297
Hyemin Park <hmjava.park@gmail.com>
2019-03-04 17:24:09 +0900

    2019.03.04   - Tibero 접속정보 추

--- CodePrismWeb/src/test/java/ktds/codeprism/web/DataSourceTest.java
+++ CodePrismWeb/src/test/java/ktds/codeprism/web/DataSourceTest.java
@@ -9,26 +9,29 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@ActiveProfiles("local")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
 public class DataSourceTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(DataSourceTest.class);
-	
+
 	@Inject
 	private DataSource ds;
-	
-	@Test
-	public void testConnection() throws Exception {
+
+	@Test 
+	public void testConnection() throws Exception { 
 		Connection conn = null;
 		try {
 			conn = ds.getConnection();
 			System.out.println( conn );
 			logger.info( conn.getCatalog() );
 			
+			logger.info( "JDK Version : " + System.getProperty("java.version") );
 			
 		}catch (Exception e) {
 			// TODO: handle exception
@@ -39,5 +42,4 @@ public class DataSourceTest {
 			}
 		}
 	}
-	
 }
